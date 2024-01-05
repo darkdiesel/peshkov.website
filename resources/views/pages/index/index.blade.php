4ea65467b5c56be5bfdfa46366fd068f918b330f
Igor Peshkov <igor.peshkov@gmail.com>
2024-01-05 04:30:46 +0300

    Add icons to main page

--- resources/views/pages/index/index.blade.php
+++ resources/views/pages/index/index.blade.php
@@ -16,7 +16,7 @@
                                 <h5 class="card-title">Igor Peshkov</h5>
                                 <p class="card-text">PHP web developer</p>
                                 <p class="card-text"><small class="text-body-secondary">WordPress, Drupal, Laravel, Yii2, CodeIgniter</small></p>
-                                <p class="card-text"><i class="fa-solid fa-location-dot"></i> Belarus, Grodno</p>
+                                <p class="card-text"><i class="fa-solid fa-location-dot fa-lg"></i> Belarus, Grodno</p>
                             </div>
                         </div>
                     </div>
@@ -27,13 +27,13 @@
                     <div class="card-body">
                         <h5 class="card-title">Contacts</h5>
                         <p class="card-text">
-                            Email: <a href="mailto:igor.peshkov@gmail.com">igor.peshkov@gmail.com</a>
+                            <i class="fa-regular fa-envelope fa-lg"></i> <a href="mailto:igor.peshkov@gmail.com">igor.peshkov@gmail.com</a>
                         </p>
                         <p class="card-text">
-                            Skype: <a href="skype:igor.peshkov.1988?chat">igor.peshkov.1988</a>
+                            <i class="fa-brands fa-skype fa-lg"></i> <a href="skype:igor.peshkov.1988?chat">igor.peshkov.1988</a>
                         </p>
                         <p class="card-text">
-                            Facebook: <a href="https://www.facebook.com/igor.peshkov.27.07.1988/" target="_blank" >igor.peshkov.27.07.1988</a>
+                            <i class="fa-brands fa-square-facebook fa-lg"></i> <a href="https://www.facebook.com/igor.peshkov.27.07.1988/" target="_blank" >igor.peshkov.27.07.1988</a>
                         </p>
                     </div>
                 </div>

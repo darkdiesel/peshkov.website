af8699aeefd529f6e7ba234c1288ee36836454b4
Igor Peshkov <igor.peshkov@gmail.com>
2024-01-28 23:46:07 +0300

    Update webpack config. Add new rules for wav file.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,21 @@ const mix = require('laravel-mix');
 
 require('laravel-mix-clean');
 
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                test: /\.wav/,
+                type: 'asset/resource',
+                generator : {
+                    filename : 'assets/audio/[hash][ext][query]',
+                    emit: true, //disable copy audio files
+                }
+            }
+        ]
+    }
+});
+
 // mix.scripts([
 //     // 'node_modules/bootstrap/dist/js/bootstrap.js',
 //     'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
@@ -24,25 +39,30 @@ require('laravel-mix-clean');
 //     'resources/js/mdb-color-modes.js',
 // ], 'public/js/main-head.min.js');
 
-mix
-    .sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/games/snake/ipa-snake-game.scss', 'public/css');
 //    .postCss('resources/css/app.css', 'public/css', [
 //        //
 //    ]);
 
 mix.copyDirectory('resources/img', 'public/img');
-//mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/fonts/fontawesome');
+// mix.copyDirectory('resources/assets/audio', 'public/assets/audio');
+// mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/fonts/fontawesome');
 
 mix.clean({
-    cleanOnceBeforeBuildPatterns: ['./js/*', './css/*', './fonts/*', './img/*'],
+    cleanOnceBeforeBuildPatterns: ['./js/*', './css/*', './fonts/*', './img/*', './audio/*' ],
 });
 
 // adding version to files
 if (mix.inProduction()) {
     mix.js('resources/js/main-head.js', 'public/js/main-head.min.js');
     mix.js('resources/js/main-footer.js', 'public/js/main-footer.min.js');
+    mix.js('resources/js/games/snake/ipa-snake-game.js', 'public/js/ipa-snake-game.min.js');
+    mix.js('resources/js/games/tetris/ipa-tetris-game.js', 'public/js/ipa-tetris-game.min.js');
     mix.version();
 } else {
     mix.js('resources/js/main-head.js', 'public/js/main-head.min.js').sourceMaps();
     mix.js('resources/js/main-footer.js', 'public/js/main-footer.min.js').sourceMaps();
+    mix.js('resources/js/games/snake/ipa-snake-game.js', 'public/js/ipa-snake-game.min.js').sourceMaps();
+    mix.js('resources/js/games/tetris/ipa-tetris-game.js', 'public/js/ipa-tetris-game.min.js').sourceMaps();
 }

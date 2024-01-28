b8af8197ee156cfc97682e4b4d01604d1d3689c2
Igor Peshkov <igor.peshkov@gmail.com>
2024-01-28 23:49:59 +0300

    Update base meta for site

--- resources/views/partials/head_meta.blade.php
+++ resources/views/partials/head_meta.blade.php
@@ -1,5 +1,10 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<meta name=”robots” content="index, follow">
+<meta name="keywords" content="dev, development, web, php, site creating, games"/>
+<meta name="author" content="Igor Peshkov">
+
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
+@stack('meta')
